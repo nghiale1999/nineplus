274295bf38fd7c9c843d152e72fd6a96064ca2af
Le Nghia <lenghia.lenghia220899@ghia220899gmail.20899gmailcom>
2022-03-09 15:31:22 +0700

    bai5

--- nineplusbai4/timkiem.php
+++ nineplusbai4/timkiem.php
@@ -8,55 +8,52 @@ if(isset($_SESSION['name'])){
     $name = $_SESSION['name'];
 }
 
-$sp_trentrang = 3;
-if(!isset($_GET['trang'])){
-    $trang=1;
-}else{
-    $trang= $_GET['trang'];
-}
-$tungtrang = ($trang-1)*$sp_trentrang;
 
+if(isset($_POST['submit'])){
+    $user = $_POST['timkiem'];
 
+    
 
-$sql = "SELECT * FROM users LIMIT $tungtrang,$sp_trentrang";
-$result = $con -> query($sql);
-$data=[];
+    $sql = "SELECT * FROM users where name='".$user."'";
+    $result = $con -> query($sql);
+    $data=[];
 
-if($result->num_rows >0){
-    while($row = $result->fetch_assoc()){
-        $data[]=$row;
+    if($result->num_rows >0){
+        while($row = $result->fetch_assoc()){
+            $data[]=$row;
+        }
     }
-}
+    
 
-$sql = "SELECT * FROM users ";
-$result = $con -> query($sql);
-$data2=[];
 
-if($result->num_rows >0){
-    while($row = $result->fetch_assoc()){
-        $data2[]=$row;
+
+    $html='';
+
+    foreach($data as $vl){
+        $html .='<tr>
+                    <td>'.$vl['id'].'</td>
+                    <td>'.$vl['name'].'</td>
+                    <td>'.$vl['email'].'</td>
+
+                    <td><Button>update</Button></td>
+                    <td><Button>delete</Button></td>
+                
+                    
+                </tr>';
     }
+
+
 }
-$tong =count($data2);
-$ttrang= ceil($tong/3);
-$i=1;
 
 
 
-$html='';
 
-foreach($data as $vl){
-    $html .='<tr>
-				<td>'.$vl['id'].'</td>
-				<td>'.$vl['name'].'</td>
-				<td>'.$vl['email'].'</td>
 
-				<td><Button>update</Button></td>
-				<td><Button>delete</Button></td>
-			
-				
-			</tr>';
-}
+
+
+
+
+
 
 ?>
 
@@ -109,12 +106,7 @@ foreach($data as $vl){
     
      
 
-<?php 
-    echo '<p>Trang:</p>';
-    for($i=1;$i<=$ttrang;$i++){       
-        echo '<a style="margin:0 5px;" href="qluser.php?trang='.$i.'">'.$i.'</a>';
-    }
- ?>
+
 
 
     <!-- Optional JavaScript -->
